754a2caf7985b755108933b1737c6f7d3c18ef9a
Alastor <ntvan.fit@gmail.com>
2019-12-10 18:19:09 +0700

    Code sơ bộ - phần vẽ đối tượng và vẽ khung cảnh

---
+++ 1712159_1712202_1712209_1712318_Lab4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _1712159_1712202_1712209_1712318_Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.bt_Cube = new System.Windows.Forms.Button();
+            this.bt_Prismatic = new System.Windows.Forms.Button();
+            this.bt_Pyramid = new System.Windows.Forms.Button();
+            this.bt_Camera = new System.Windows.Forms.Button();
+            this.bt_Coordinates = new System.Windows.Forms.Button();
+            this.bt_Plane = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(3, 58);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(1074, 558);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openGLControl.Load += new System.EventHandler(this.openGLControl1_Load);
+            this.openGLControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.openGLControl_KeyDown);
+            this.openGLControl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.openGLControl_KeyPress);
+            this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseDown);
+            this.openGLControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseMove);
+            this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseUp);
+            this.openGLControl.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.openGLControl_PreviewKeyDown);
+            // 
+            // bt_Cube
+            // 
+            this.bt_Cube.Location = new System.Drawing.Point(12, 3);
+            this.bt_Cube.Name = "bt_Cube";
+            this.bt_Cube.Size = new System.Drawing.Size(75, 23);
+            this.bt_Cube.TabIndex = 1;
+            this.bt_Cube.Text = "Cube";
+            this.bt_Cube.UseVisualStyleBackColor = true;
+            this.bt_Cube.Click += new System.EventHandler(this.bt_Cube_Click);
+            // 
+            // bt_Prismatic
+            // 
+            this.bt_Prismatic.Location = new System.Drawing.Point(13, 33);
+            this.bt_Prismatic.Name = "bt_Prismatic";
+            this.bt_Prismatic.Size = new System.Drawing.Size(75, 23);
+            this.bt_Prismatic.TabIndex = 2;
+            this.bt_Prismatic.Text = "Prismatic";
+            this.bt_Prismatic.UseVisualStyleBackColor = true;
+            this.bt_Prismatic.Click += new System.EventHandler(this.bt_Prismatic_Click);
+            // 
+            // bt_Pyramid
+            // 
+            this.bt_Pyramid.Location = new System.Drawing.Point(93, 3);
+            this.bt_Pyramid.Name = "bt_Pyramid";
+            this.bt_Pyramid.Size = new System.Drawing.Size(75, 23);
+            this.bt_Pyramid.TabIndex = 3;
+            this.bt_Pyramid.Text = "Pyramid";
+            this.bt_Pyramid.UseVisualStyleBackColor = true;
+            this.bt_Pyramid.Click += new System.EventHandler(this.bt_Pyramid_Click);
+            // 
+            // bt_Camera
+            // 
+            this.bt_Camera.Location = new System.Drawing.Point(93, 33);
+            this.bt_Camera.Name = "bt_Camera";
+            this.bt_Camera.Size = new System.Drawing.Size(75, 23);
+            this.bt_Camera.TabIndex = 4;
+            this.bt_Camera.Text = "Camera";
+            this.bt_Camera.UseVisualStyleBackColor = true;
+            this.bt_Camera.Click += new System.EventHandler(this.bt_Camera_Click);
+            // 
+            // bt_Coordinates
+            // 
+            this.bt_Coordinates.Location = new System.Drawing.Point(988, 3);
+            this.bt_Coordinates.Name = "bt_Coordinates";
+            this.bt_Coordinates.Size = new System.Drawing.Size(75, 23);
+            this.bt_Coordinates.TabIndex = 5;
+            this.bt_Coordinates.Text = "Coordinate";
+            this.bt_Coordinates.UseVisualStyleBackColor = true;
+            this.bt_Coordinates.Click += new System.EventHandler(this.bt_Coordinates_Click);
+            // 
+            // bt_Plane
+            // 
+            this.bt_Plane.Location = new System.Drawing.Point(988, 33);
+            this.bt_Plane.Name = "bt_Plane";
+            this.bt_Plane.Size = new System.Drawing.Size(75, 23);
+            this.bt_Plane.TabIndex = 6;
+            this.bt_Plane.Text = "Plane";
+            this.bt_Plane.UseVisualStyleBackColor = true;
+            this.bt_Plane.Click += new System.EventHandler(this.bt_Plane_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 617);
+            this.Controls.Add(this.bt_Plane);
+            this.Controls.Add(this.bt_Coordinates);
+            this.Controls.Add(this.bt_Camera);
+            this.Controls.Add(this.bt_Pyramid);
+            this.Controls.Add(this.bt_Prismatic);
+            this.Controls.Add(this.bt_Cube);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Button bt_Cube;
+        private System.Windows.Forms.Button bt_Prismatic;
+        private System.Windows.Forms.Button bt_Pyramid;
+        private System.Windows.Forms.Button bt_Camera;
+        private System.Windows.Forms.Button bt_Coordinates;
+        private System.Windows.Forms.Button bt_Plane;
+    }
+}
+
---
+++ 1712159_1712202_1712209_1712318_Lab4/Form1.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace _1712159_1712202_1712209_1712318_Lab4
+{
+    public partial class Form1 : Form
+    {
+        int shshape = 1;
+        Point pStart, pEnd;
+        bool mouseDown, mouseUp, camera = false, coordinate = false, plane = false;
+        float height;
+        int key;
+        float cx = 3.0f, cz = 7.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+
+            float[] light_pos = { 1.0f, 0.0f, 0.0f, 0.0f };
+            gl.ClearColor(0, 0, 0, 0);
+            gl.ShadeModel(OpenGL.GL_SMOOTH);
+            float shininess = 50.0f;
+
+            gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SHININESS, shininess);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_POSITION, light_pos);
+
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_LIGHT0);
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+        }        
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            
+                OpenGL gl = openGLControl.OpenGL;
+
+                //set ma tran viewport
+                gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
+
+                //set ma tran phep chieu
+                gl.MatrixMode(OpenGL.GL_PROJECTION);
+                gl.Perspective(45, openGLControl.Width / openGLControl.Height, 1.0, 60.0);
+
+                //set ma tran model view
+                gl.MatrixMode(OpenGL.GL_MODELVIEW);
+                gl.LookAt(2.0f, 5.0f, 6.0f, 0.0, 0.0f, 0.0, 0.0f, 1.0f, 0.0f);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            
+            if (coordinate==true)
+            {
+                drawCoor(gl);
+            }
+            if(plane==true)
+            {
+                drawPlane(gl,height);
+            }
+            // Draw a coloured cube
+            switch (shshape)
+            {
+                case 1:
+                    drawCube(gl, pStart, pEnd);
+                    break;
+                case 2:
+                    drawPrismatic(gl, pStart, pEnd);
+                    break;
+                case 3:
+                    drawPyramid(gl, pStart, pEnd);
+                    break;
+            }
+        }
+
+        //Function to draw shape
+        private void drawCube(OpenGL gl, Point pStart, Point pEnd)
+        {
+            float a;
+            a= (float) Math.Sqrt(Math.Pow(pEnd.X-pStart.X, 2) + Math.Pow(pEnd.Y - pStart.Y, 2));
+            a = a / (float)Math.Sqrt(2);
+            gl.Color(255.0, 255.0, 255.0);
+
+            height = a/2;
+            gl.Begin(OpenGL.GL_QUADS);
+
+            //Bottom
+            gl.Normal(0.0, -1.0, 0.0);
+            gl.Vertex(a / 2, -a/2, a / 2);
+            gl.Vertex(a / 2, -a/2, -a / 2);
+            gl.Vertex(-a / 2, -a / 2, -a / 2);
+            gl.Vertex(-a / 2, -a / 2, a / 2);
+
+            //Top
+            gl.Normal(0.0, 1.0, 0.0);
+            gl.Vertex(a / 2, a / 2, a / 2);
+            gl.Vertex(a / 2, a / 2, -a / 2);
+            gl.Vertex(-a / 2, a / 2, -a / 2);
+            gl.Vertex(-a / 2, a / 2, a / 2);
+
+            //Front
+            gl.Normal(0.0, 0.0, 1.0);
+            gl.Vertex(a / 2, a / 2, a / 2);
+            gl.Vertex(a / 2, -a / 2, a / 2);
+            gl.Vertex(-a / 2, -a / 2, a / 2);
+            gl.Vertex(-a / 2, a / 2, a / 2);
+
+            //Back
+            gl.Normal(0.0, 0.0, -1.0);
+            gl.Vertex(a / 2, a / 2, -a / 2);
+            gl.Vertex(a / 2, -a / 2, -a / 2);
+            gl.Vertex(-a / 2, -a / 2, -a / 2);
+            gl.Vertex(-a / 2, a / 2, -a / 2);
+
+            //Left
+            gl.Normal(-1.0, 0.0, 0.0);
+            gl.Vertex(-a / 2, a / 2, -a / 2);
+            gl.Vertex(-a / 2, -a / 2, -a / 2);
+            gl.Vertex(-a / 2, -a / 2, a / 2);
+            gl.Vertex(-a / 2, a / 2, a / 2);
+
+            //Right
+            gl.Normal(1.0, 0.0, 0.0);
+            gl.Vertex(a / 2, a / 2, -a / 2);
+            gl.Vertex(a / 2, -a / 2, -a / 2);
+            gl.Vertex(a / 2, -a / 2, a / 2);
+            gl.Vertex(a / 2, a / 2, a / 2);
+
+            gl.End();
+            gl.Flush();
+        }
+
+        private void drawPrismatic(OpenGL gl, Point pStart, Point pEnd)
+        {
+            float a, a1, a2, b;
+            a= Math.Abs(pStart.X - pEnd.X);
+            a1 = a * (float)Math.Sqrt(3) / 3.0f;
+            a2 = a * (float)Math.Sqrt(3) / 6.0f;
+            b = Math.Abs(pStart.Y - pEnd.Y);
+            height = b / 2;
+
+            gl.Color(255.0, 255.0, 255.0);
+
+            gl.Begin(OpenGL.GL_TRIANGLES); //Bottom
+
+            gl.Normal(0.0, -1.0, 0.0);
+            gl.Vertex(0.0, -b / 2, a1);
+            gl.Vertex(a / 2, -b / 2, -a2);
+            gl.Vertex(-a / 2, -b / 2, -a2);
+
+            //Top
+            gl.Normal(0.0, 1.0, 0.0);
+            gl.Vertex(0.0, b / 2, a1);
+            gl.Vertex(a / 2, b / 2, -a2);
+            gl.Vertex(-a / 2, b / 2, -a2);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_QUADS);
+            //Right
+            gl.Normal(1.0, 0.0, 0.0);
+            gl.Vertex(0.0, -b / 2, a1);
+            gl.Vertex(a / 2, -b / 2, -a2);
+            gl.Vertex(a / 2, b / 2, -a2);
+            gl.Vertex(0.0, b / 2, a1);
+
+            //Back
+            gl.Normal(0.0, 0.0, -1.0);
+            gl.Vertex(a / 2, -b / 2, -a2);
+            gl.Vertex(-a / 2, -b / 2, -a2);
+            gl.Vertex(-a / 2, b / 2, -a2);
+            gl.Vertex(a / 2, b / 2, -a2);
+
+            //Left
+            gl.Normal(-1.0, 0.0, 0.0);
+            gl.Vertex(0.0, -b / 2, a1);
+            gl.Vertex(-a / 2, -b / 2, -a2);
+            gl.Vertex(-a / 2, b / 2, -a2);
+            gl.Vertex(0.0, b / 2, a1);
+            
+            gl.End();
+
+            gl.Flush();
+        }
+
+        private void drawPyramid(OpenGL gl, Point pStart, Point pEnd)
+        {
+            float a, b;
+            a = Math.Abs(pEnd.X - pStart.X) / 2;
+            b = Math.Abs(pEnd.Y - pStart.Y) / 2;
+            float R;
+            R = ((3 * a * a + 4 * b * b) / 4) / (2 * b);
+            height = R - b;
+
+            gl.Color(255.0, 255.0, 255.0);
+
+            gl.Begin(OpenGL.GL_QUADS); //Bottom
+            gl.Normal(0.0, -1.0, 0.0);
+            gl.Vertex(a / 2, R - b, a / 2);
+            gl.Vertex(a / 2, R - b, -a / 2);
+            gl.Vertex(-a / 2, R - b, -a / 2);
+            gl.Vertex(-a / 2, R - b, a / 2);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            gl.Normal(1.0, 0.0, 0.0);
+            gl.Vertex(0.0, R, 0.0);
+            gl.Vertex(a / 2, R - b, a / 2);
+            gl.Vertex(a / 2, R - b, -a / 2);
+
+            gl.Normal(0.0, 0.0, -1.0);
+            gl.Vertex(0.0, R, 0.0);
+            gl.Vertex(a / 2, R - b, -a / 2);
+            gl.Vertex(-a / 2, R - b, -a / 2);
+
+            gl.Normal(-1.0, 0.0, 0.0);
+            gl.Vertex(0.0, R, 0.0);
+            gl.Vertex(-a / 2, R - b, -a / 2);
+            gl.Vertex(-a / 2, R - b, a / 2);
+
+            gl.Normal(0.0, 0.0, 1.0);
+            gl.Vertex(0.0, R, 0.0);
+            gl.Vertex(-a / 2, R - b, a / 2);
+            gl.Vertex(a / 2, R - b, a / 2);
+
+            gl.End();
+            gl.Flush();
+        }
+
+        private void drawCoor(OpenGL gl)
+        {
+            gl.LineWidth(3.0f);
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Color(1.0f, 0.0, 0.0);
+            gl.Vertex(100, 0.0, 0.0);
+            gl.Vertex(-100, 0.0, 0.0);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Color(0.0, 1.0, 0.0);
+            gl.Vertex(0.0, -100.0, 0.0);
+            gl.Vertex(0.0, 100.0, 0.0);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Color(0.0, 0.0, 1.0);
+            gl.Vertex(0.0, 0.0, -100.0);
+            gl.Vertex(0.0, 0.0, 100.0);
+            gl.End();
+
+            gl.Flush();
+            gl.Enable(OpenGL.GL_LIGHTING);
+        }
+
+        private void drawPlane(OpenGL gl, float h)
+        {
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.LineWidth(0.01f);
+            gl.Color(255.0, 255.0, 255.0);
+            gl.Begin(OpenGL.GL_LINES);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(i+500, -h, 100);
+                gl.Vertex(i+500, -h, -100);
+                gl.End();
+
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(i-500, -h, 100);
+                gl.Vertex(i-500, -h, -100);
+                gl.End();
+            }
+            gl.End();
+
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(100, -h, i+500);
+                gl.Vertex(-100, -h, i+500);
+                gl.End();
+
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(100, -h, i - 500);
+                gl.Vertex(-100, -h, i - 500);
+                gl.End();
+            }
+            
+            gl.Flush();
+            gl.Enable(OpenGL.GL_LIGHTING);
+
+        }
+        //Envet of key down
+        private void openGLControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            key = e.KeyValue;
+        }
+
+        //Event of button click
+        private void bt_Cube_Click(object sender, EventArgs e)
+        {
+            shshape = 1;
+        }
+
+        private void bt_Prismatic_Click(object sender, EventArgs e)
+        {
+            shshape = 2;
+        }
+
+        private void bt_Pyramid_Click(object sender, EventArgs e)
+        {
+            shshape = 3;
+        }
+
+        private void bt_Camera_Click(object sender, EventArgs e)
+        {
+            camera = true;
+        }
+
+        private void openGLControl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void openGLControl_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            
+        }
+
+        private void bt_Plane_Click(object sender, EventArgs e)
+        {
+            if (plane == false)
+            {
+                plane = true;
+            }
+            else
+                plane = false;
+        }
+
+        private void bt_Coordinates_Click(object sender, EventArgs e)
+        {
+            if (coordinate == false)
+            {
+                coordinate = true;
+            }
+            else
+                coordinate = false;
+        }
+
+
+        //Envent of mouse 
+        private void openGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            pStart = e.Location;
+            pEnd = pStart;
+            mouseDown = true;
+            mouseUp = false;
+        }
+
+        private void openGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseUp == false && mouseDown == true)
+            {
+                pEnd = e.Location;
+            }
+        }
+
+        private void openGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            pEnd = e.Location;
+            mouseDown = false;
+            mouseUp = true;
+        }
+
+        //---------------------------------------------------------------------------------------------------------
+        public class CameraObjcet : Form1
+        {
+            float _left;
+            float _right;
+
+            public CameraObjcet()
+            {
+                _left = cx;
+                _right = cz;
+            }
+
+            public CameraObjcet(float x, float z)
+            {
+                _left = x;
+                _right = z;
+            }
+
+            public float getLeft()
+            {
+                return _left;
+            }
+
+            public float getRight()
+            {
+                return _right;
+            }
+
+            public void getLeft(float x)
+            {
+                _left = x;
+            }
+
+            public void getRight(float z)
+            {
+                _right = z;
+            }
+
+            public void KeyValue(int key)
+            {
+                switch(key)
+                {
+                    case (int)Keys.Left:
+                        _left -= 0.5f;
+                        break;
+                    case (int)Keys.Right:
+                        _left += 0.5f;
+                        break;
+                }
+            }
+        }
+    }
+}
